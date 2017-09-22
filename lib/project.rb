0fe047c96451d3c4e7883c7fad07605de0b76518
srhcrete <srhcrete@gmail.com>
2017-09-22 14:13:52 -0700

    pass test for project delete

--- lib/project.rb
+++ lib/project.rb
@@ -50,4 +50,7 @@ class Project
     DB.exec("UPDATE projects SET title = '#{@title}' WHERE id =#{id};")
   end
 
+  def delete
+    DB.exec("DELETE FROM projects where id = #{self.id()};")
+  end
 end
