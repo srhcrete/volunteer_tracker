102e7a1c24c6372f74e6ab590d53db31f09bb17f
srhcrete <srhcrete@gmail.com>
2017-09-22 12:14:00 -0700

    pass test for project volunteers, volunteer save and ==

--- lib/project.rb
+++ lib/project.rb
@@ -31,4 +31,16 @@ class Project
     name = result.first().fetch("title")
     Project.new({:title => name, :id => id})
   end
+
+  def volunteers
+    project_volunteers = []
+    volunteers = DB.exec("SELECT * FROM volunteers where project_id = #{self.id};")
+    volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i()
+      project_volunteers.push(Volunteer.new({:name => name, :project_id => project_id}))
+    end
+    project_volunteers
+  end
+
 end
--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,3 +1,19 @@
 class Volunteer
+  attr_reader(:name, :id)
+  attr_accessor(:project_id)
 
-end  
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = nil
+  end
+
+  def save()
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', '#{@project_id}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  def ==(another_volunteer)
+    self.name().==(another_volunteer.name())
+  end
+end
